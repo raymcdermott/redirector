364a99f160580e6e30b687b2bb59d591ed2730a6
Ray McDermott <ray.mcdermott@toyota-europe.com>
2014-09-17 00:47:28 +0200

    works with REDIS and Mongo

--- src/redirector/web.clj
+++ src/redirector/web.clj
@@ -2,6 +2,7 @@
   (:require [compojure.core :refer [defroutes GET ANY]]
             [compojure.handler :refer [site]]
             [compojure.route :as route]
+            [ring.util.response :as response]
             [ring.adapter.jetty :as jetty]
             [monger.core :as mg]
             [monger.collection :as mc]
@@ -13,78 +14,67 @@
 ;
 ; read data from Mongo ... source of truth
 
-(def mongo-url (or "mongodb://localhost" (env :MONGO_URL)))
+(def mongo-uri (or "mongodb://localhost/test" (env :MONGO_URL)))
 (def mongo-collection (or "redirections" (env :MONGO_COLLECTION)))
 
-(defn mongo-action
-  "Run a data function on MongoDB wrapped by a connect / disconnect"
-  ([db-fn data]
-   (mongo-action mongo-url db-fn data))
+(defn mongo-query [data fields]
+  (let [{:keys [conn db]} (mg/connect-via-uri mongo-uri)
+        result (mc/find-one-as-map db mongo-collection data fields)]
+    (mg/disconnect conn)
+    result))
 
-  ([db-uri db-fn data]
-   {:pre [(string? db-uri)]}
-   (mongo-action db-uri mongo-collection db-fn data))
-
-  ([db-uri collection db-fn data]
-   {:pre [(string? db-uri) (string? collection)]}
-   (let [{:keys [conn db]} (mg/connect-via-uri db-uri)
-         result (db-fn db collection data)]
-     (mg/disconnect conn)
-     result)))
-
-(defn get-configuration-from-mongo [brand country]
-  "Obtain the redirect configuration data from MongoDB for country"
-  (let [redirection-data (mongo-action mc/find-one-as-map {:brand brand, :country country} [:domain :bucket])]
-    (if (nil? redirection-data)
+(defn get-route-from-mongo [brand country]
+  "Obtain the redirect configuration data from MongoDB for brand / country"
+  (let [{:keys [domain bucket]} (mongo-query {:brand brand :country country} [:domain :bucket])]
+    (if (nil? domain)
       (throw (Exception. (str "Failed, cannot find cache domain for : " country)))
-      (str (:domain redirection-data) "/" (:bucket redirection-data)))))
+      (str domain "/" bucket))))
 
 ; -------*** REDIS HELPERS ... push out to another file
 ;
 ; read data from REDIS ... source of speed
 
-(def redis-url (or "redis:default:2600" (env :REDIS_URL)))
-(def redis-conn {:pool {} :spec {:uri redis-url}})
+;(def redis-url (or "127.0.0.1" (env :REDIS_URL))) ; TBC
+(def redis-conn {:pool {} :spec {:host "127.0.0.1" :port 6379}})
 (defmacro wcar* [& body] `(car/wcar redis-conn ~@body))
 
 ; set a default of 30 seconds for data expiry in the REDIS cache
 (def redis-ttl (or (env :REDIS_TTL_SECONDS) 30))
 
-(defn setkv
+(defn set-route-in-cache!
   ([k v]
-   (wcar* (car/set k v redis-ttl)))
+   (set-route-in-cache! k v redis-ttl))
   ([k v ttl]
-   (wcar* (car/set k v ttl))))
+   (wcar* (car/setex k ttl v))))
 
-(defn getv [k]
+(defn get-route-from-cache [k]
   (wcar* (car/get k)))
 
 ; -------*** WORK
 ;
 ; get data from REDIS or get data from Mongo
-
+;
 ; if data OK return 301 with new URL from the domain cache
-; if no data, 404
 ; if exception 500
 
-(defn get-route
-  ([country]
-   (country "TOYOTA"))
-  ([brand country]
-   (get-configuration-from-mongo brand country)))
+(defn get-route [brand country]
+  (if-let [cached-route (get-route-from-cache (str brand country))]
+    cached-route
+    (let [route (get-route-from-mongo brand country)]
+      (set-route-in-cache! (str brand country) route)
+      (prn "not cached, obtained from mongo")
+      route)))
 
-(defn respond [country resource]
-  {:status 200
-   :body   (get-route country)})
-
+(defn respond [brand country resource]
+  (str (get-route brand country) "/" resource))
 
 
 ; -------*** EXPOSE TO THE WEB
 ;
 
 (defroutes app
-           (GET "/:country/*" [country *]
-                (time (respond country *)))
+           (GET "/:brand/:country/*" [brand country *]
+                (time (response/redirect (respond brand country *))))
            (ANY "*" []
                 (route/not-found (slurp (io/resource "404.html")))))
 
