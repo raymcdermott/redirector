4e5684686a604f8c056a781eebc0a84a592e5e8a
Ray McDermott <ray@mcdermott.be>
2014-09-22 11:54:31 +0200

    Catch up git

--- project.clj
+++ project.clj
@@ -8,7 +8,7 @@
                  [ring/ring-jetty-adapter "1.2.2"]
                  [com.novemberain/monger "2.0.0"]
                  [com.taoensso/carmine "2.7.0"]
-                 [environ "0.5.0"]]
+                 [environ "1.0.0"]]
   :min-lein-version "2.0.0"
   :plugins [[environ/environ.lein "0.2.1"]]
   :hooks [environ.leiningen.hooks]
